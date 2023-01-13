44152ace384569d5eda2b721e377ed8b59444f95
marlenne798301 <118927207+marlenne798301@users.noreply.github.com>
2023-01-13 14:41:26 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>MI PRACTICA CON CODESANBOX</h1>
+<h2>MARLENNE IMELDA IBARRA ORTEGA</h2>
+<div>
+ <h3> 
+ UNIVERSIDAD TECNOLOGICA DE DURANGO
+ </h3>
+  <a href="https://github.com/marlenne798301/Contador1" target="_blank" rel="noopener noreferrer">MI GITHUB</a>.
+</div>
+`;
